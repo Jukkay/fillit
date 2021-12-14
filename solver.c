623ec6ec347620a7b5ad7fab1d4a5bba2b2199a5
Henri Tahvanainen <htahvana@c1r4p3.hive.fi>
2021-12-14 15:19:17 +0200

    added sandbox long maker for bit comparisons, added empty file for solver

--- solver.c
+++ solver.c
@@ -1,30 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   fillit.h                                           :+:      :+:    :+:   */
+/*   solver.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: htahvana <htahvana@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/12/13 12:04:21 by htahvana          #+#    #+#             */
-/*   Updated: 2021/12/14 15:16:49 by htahvana         ###   ########.fr       */
+/*   Created: 2021/12/14 13:56:48 by htahvana          #+#    #+#             */
+/*   Updated: 2021/12/14 13:57:34 by htahvana         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef FILLIT_H
-# define FILLIT_H
-# include "libft.h"
-# include "ft_point.h"
-
-typedef struct s_tetris
-{
-	unsigned short shape;
-	t_point *pos;
-
-}	t_tetris;
-
-unsigned short		savetoshort(char *square);
-int					validgrid(char *grid);
-int					ft_read_file(int argc, char **argv);
-unsigned long long	sandboxshort(unsigned short i, int x, int y);
-
-#endif
