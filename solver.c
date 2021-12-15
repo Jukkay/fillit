8af6ce381aaf043652cc9d24abbfd860b089d49c
Henri Tahvanainen <htahvana@c1r4p3.hive.fi>
2021-12-15 11:50:16 +0200

    added collisioncheck to solver

--- solver.c
+++ solver.c
@@ -6,27 +6,39 @@
 /*   By: htahvana <htahvana@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/14 13:56:48 by htahvana          #+#    #+#             */
-/*   Updated: 2021/12/15 10:30:26 by htahvana         ###   ########.fr       */
+/*   Updated: 2021/12/15 11:48:46 by htahvana         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fillit.h"
 
 
-int	shortcompare(t_tetris *t1, t_tetris *t2)
+/* int	shortcompare(t_tetris *t1, t_tetris *t2)
 {
 	return (!(sbshort(t1->shape, 0, 0)
 	& (sbshort(t2->shape, t2->pos->x, t2->pos->y))));
-}
+} */
 
-/* int collisioncheck(t_point curpos, t_tetris *current, t_tetris ***alltetri, int boxwidth)
+int collisioncheck(t_point curpos, t_tetris *current, t_tetris **alltetri, int boxwidth)
 {
-	//check if curpos.x + current.size.x > boxwidth
-	//check if curpos.y + current.size.y > boxwidth
-
+	int l;
+	
+	l = 0;
+	if(curpos.x + current->size->x > boxwidth)
+		return (0);
+	if(curpos.y + current->size->y > boxwidth)
+		return (0);
+	while(alltetri[l] && alltetri[l]->pos->x >= 0)
+	{
+		if(sbshort(current, 0, 0) & sbshort(alltetri[l]->shape, \
+			curpos.x - alltetri[l]->pos->x, curpos.y - alltetri[l]->pos->y))
+			return (0);
+		l++;
+	}
+	return (1);
 }
 
-int	solver(t_tetris ***tetri, t_point *start)
+/* int	solver(t_tetris ***tetri, t_point *start)
 {
 
 	int	result = 0;
@@ -43,8 +55,8 @@ int	solver(t_tetris ***tetri, t_point *start)
 	}
 
 	return (0)
-} */
-
+}
+ */
 int solve_tetris(t_tetris ***tetri)
 {
 	int minsize;
@@ -54,6 +66,10 @@ int solve_tetris(t_tetris ***tetri)
 	start->y = 0;
 
 	minsize = smallest(tetri);
-	solver(tetri, start);
+	while(!solver(tetri, start))
+	{
+		minsize++;
+	}
+)
 
 }
