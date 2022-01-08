bed957881f123119bcf09d69005b99c1e2b1c0cd
htahvana <htahvana@student.hive.fi>
2022-01-08 21:14:55 +0200

    added optimization to find the next potential free position

--- map.c
+++ map.c
@@ -6,7 +6,7 @@
 /*   By: htahvana <htahvana@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/08 01:49:05 by htahvana          #+#    #+#             */
-/*   Updated: 2022/01/08 06:29:22 by htahvana         ###   ########.fr       */
+/*   Updated: 2022/01/08 21:12:55 by htahvana         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,60 +14,59 @@
 
 void	printtomap(int i, t_tetris *alltetri, t_uint16 *map)
 {
-t_uint16		mask;
+	t_uint16	mask;
 
 	mask = 61440;
 	map[alltetri[i].pos->y] |= ((alltetri[i].shape & mask)
-		>> alltetri[i].pos->x);
+			>> alltetri[i].pos->x);
 	mask = 3840;
 	map[alltetri[i].pos->y + 1] |= ((alltetri[i].shape & mask)
-		<< 4 >> alltetri[i].pos->x);
+			<< 4 >> alltetri[i].pos->x);
 	mask = 240;
 	map[alltetri[i].pos->y + 2] |= ((alltetri[i].shape & mask)
-		<< 8 >> alltetri[i].pos->x);
+			<< 8 >> alltetri[i].pos->x);
 	mask = 15;
 	map[alltetri[i].pos->y + 3] |= ((alltetri[i].shape & mask)
-		<< 12 >> alltetri[i].pos->x);
+			<< 12 >> alltetri[i].pos->x);
 }
 
 void	remofmap(int i, t_tetris *alltetri, t_uint16 *map)
 {
-	t_uint16		mask;
+	t_uint16	mask;
 
 	mask = 61440;
 	map[alltetri[i].pos->y] ^= ((alltetri[i].shape & mask)
-		>> alltetri[i].pos->x);
+			>> alltetri[i].pos->x);
 	mask = 3840;
 	map[alltetri[i].pos->y + 1] ^= ((alltetri[i].shape & mask)
-		<< 4 >> alltetri[i].pos->x);
+			<< 4 >> alltetri[i].pos->x);
 	mask = 240;
 	map[alltetri[i].pos->y + 2] ^= ((alltetri[i].shape & mask)
-		<< 8 >> alltetri[i].pos->x);
+			<< 8 >> alltetri[i].pos->x);
 	mask = 15;
 	map[alltetri[i].pos->y + 3] ^= ((alltetri[i].shape & mask)
-		<< 12 >> alltetri[i].pos->x);
+			<< 12 >> alltetri[i].pos->x);
 }
 
 int	collisioncheck(int i, t_tetris *alltetri, t_uint16 *map)
 {
-	t_uint16		mask;
+	t_uint16	mask;
 
 	mask = 61440;
-		if (map[alltetri[i].pos->y]
-			& ((alltetri[i].shape & mask) >> alltetri[i].pos->x))
-			return (0);
+	if (map[alltetri[i].pos->y]
+		& ((alltetri[i].shape & mask) >> alltetri[i].pos->x))
+		return (0);
 	mask = 3840;
-		if (map[alltetri[i].pos->y + 1]
-			& ((alltetri[i].shape & mask) << 4 >> alltetri[i].pos->x))
-			return (0);
+	if (map[alltetri[i].pos->y + 1]
+		& ((alltetri[i].shape & mask) << 4 >> alltetri[i].pos->x))
+		return (0);
 	mask = 240;
-		if (map[alltetri[i].pos->y + 2]
-			& ((alltetri[i].shape & mask) << 8 >> alltetri[i].pos->x))
-			return (0);
+	if (map[alltetri[i].pos->y + 2]
+		& ((alltetri[i].shape & mask) << 8 >> alltetri[i].pos->x))
+		return (0);
 	mask = 15;
-		if (map[alltetri[i].pos->y + 3]
-			& ((alltetri[i].shape & mask) << 12 >> alltetri[i].pos->x))
-			return (0);
+	if (map[alltetri[i].pos->y + 3]
+		& ((alltetri[i].shape & mask) << 12 >> alltetri[i].pos->x))
+		return (0);
 	return (1);
 }
-
--- solver.c
+++ solver.c
@@ -6,7 +6,7 @@
 /*   By: htahvana <htahvana@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/06 21:40:45 by htahvana          #+#    #+#             */
-/*   Updated: 2022/01/08 06:25:26 by htahvana         ###   ########.fr       */
+/*   Updated: 2022/01/08 21:08:52 by htahvana         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,16 +15,25 @@
 static int	move_tpos(int i, t_tetris *alltetri, int boxwidth, t_uint16 *map)
 {
 	t_uint16	mask;
-	mask = 65535 << (16 - boxwidth);
-	if ((map[alltetri[i].pos->y] ^ mask)
-		&& (alltetri[i].pos->x + 1) + alltetri[i].size->x <= boxwidth)
-			alltetri[i].pos->x++;
-	else if (alltetri[i].pos->y + alltetri[i].size->y < boxwidth)
-		ft_setpoint(alltetri[i].pos, 0, alltetri[i].pos->y + 1);
-	else
+
+	alltetri[i].pos->x++;
+		mask = 57344 >> alltetri[i].pos->x;
+	while (((map[alltetri[i].pos->y] & mask) ^ mask) == 0)
 	{
-		ft_setpoint(alltetri[i].pos, -1, -1);
-		return (0);
+		if ((alltetri[i].pos->x) + alltetri[i].size->x < boxwidth)
+		{
+			alltetri[i].pos->x++;
+		}
+		else if (alltetri[i].pos->y + alltetri[i].size->y < boxwidth)
+		{
+			ft_setpoint(alltetri[i].pos, 0, alltetri[i].pos->y + 1);
+		}		
+		else
+		{
+			ft_setpoint(alltetri[i].pos, -1, -1);
+			return (0);
+		}
+		mask = 57344 >> alltetri[i].pos->x;
 	}
 	return (1);
 }
@@ -55,6 +64,18 @@ static int	solver(t_tetris *alltetri, int boxwidth, t_uint16 *map)
 	return (0);
 }
 
+static void	initmap(int boxwidth, t_uint16 *map)
+{
+	int	i;
+
+	i = -1;
+	while (++i < boxwidth)
+	{
+		map[i] = 65535 >> boxwidth;
+	}
+	map[i] = 65535;
+}
+
 int	solve_tetris(t_tetris *tetri)
 {
 	int			minsize;
@@ -70,14 +91,11 @@ int	solve_tetris(t_tetris *tetri)
 	while (minsize * minsize < i * 4)
 		minsize++;
 	i = -1;
-	while (++i < 16)
-		map[i] = (t_uint16)0;
+	initmap(minsize, map);
 	while (solver(tetri, minsize, map) == 0)
 	{
-		i = -1;
-		while (++i < 16)
-			map[i] = (t_uint16)0;
 		minsize++;
+		initmap(minsize, map);
 	}
 	return (minsize);
 }
